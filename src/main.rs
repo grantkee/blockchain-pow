1240dc08d99c306a79a6b35797204bffd012717f
grantkee <grant@ether.ai>
2022-04-20 13:13:50 -0600

    added useful crates and structs for block and app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use uuid::Uuid;
+use serde::{Serialize, Deserialize};
+
+pub struct App {
+    pub blockchain: Vec<Block>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+    pub id: Uuid,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp: i64,
+    pub data: String,
+    pub nonce: u64,
+}
+
 fn main() {
     println!("Hello, world!");
 }
